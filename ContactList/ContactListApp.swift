72654eacda954f6ab3bdd641a79954dcfc2bf0b3
NikolayD <volodar86@yandex.ru>
2024-09-23 12:55:26 +0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by NikolayD on 23.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
